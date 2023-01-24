171bf902d7a9a30572bef881518bdfad3ff9f2bc
Mikhail Ledovskikh <mishasvb@mail.ru>
2023-01-24 22:17:08 +0900

    Внес правки

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -1,15 +1,22 @@
 let openMenu = document.querySelector('.header__menu');
 let closeMenu = document.querySelector('.header__back');
 let mobileMenu = document.querySelector('.body__mobileMenu');
+let overlay = document.querySelector('.overlay');
 
 openMenu.addEventListener('click', function (){
   mobileMenu.classList.add('body__mobileMenu-open');
+  overlay.style.display = 'block';
 });
 
 closeMenu.addEventListener('click', function (){
   mobileMenu.classList.remove('body__mobileMenu-open');
+  overlay.style.display = 'none';
 });
 
-console.log(mobileMenu);
+overlay.addEventListener('click', function(){
+  mobileMenu.classList.remove('body__mobileMenu-open');
+  overlay.style.display = 'none';
+});
+
 
 
--- src/js/popup-call.js
+++ src/js/popup-call.js
@@ -2,7 +2,7 @@ let btnOpenCall = document.querySelectorAll('.contacts__call');
 let btnOrderCall = document.querySelectorAll('.contacts__chat');
 let btnCloseCall = document.querySelectorAll('.header__back--request');
 let overlay = document.querySelector('.overlay');
-
+let mobileMenu = document.querySelector('.body__mobileMenu');
 let popupCall = document.querySelector('.body__mobileMenu--request'); 
 let orderCall = document.querySelector('.order-call');
 
@@ -11,13 +11,15 @@ let orderCall = document.querySelector('.order-call');
 for (let i = 0; i < btnOrderCall.length; i++){
   btnOrderCall[i].addEventListener('click', function (){
     popupCall.classList.toggle('open-popup');
+    mobileMenu.classList.remove('body__mobileMenu-open');
     overlay.style.display = 'block';
   })  
 }
 
 for (let i = 0; i < btnOpenCall.length; i++){
   btnOpenCall[i].addEventListener('click', function (){
-    orderCall.classList.toggle('open-popup');    
+    orderCall.classList.toggle('open-popup');  
+    mobileMenu.classList.remove('body__mobileMenu-open');  
     overlay.style.display = 'block';
   })  
 }
